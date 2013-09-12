eeb703789fabecc2e9f47b98fbc1c1bd7540ee77
WoLpH <Rick@Fawo.nl>
2013-09-12 18:12:13 +0200

    fixed read the docs?

--- docs/conf.py
+++ docs/conf.py
@@ -14,9 +14,6 @@
 import sys, os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
 
-from django.conf import settings
-settings.configure()
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
