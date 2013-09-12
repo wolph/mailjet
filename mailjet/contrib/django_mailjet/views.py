c3a97058499b090696f0fb88480f1e5827e5bb79
jpic <jamespic@gmail.com>
2013-09-12 17:48:00 +0200

    Fixed template name in SubscriptionView

--- mailjet/contrib/django_mailjet/views.py
+++ mailjet/contrib/django_mailjet/views.py
@@ -9,7 +9,7 @@ from .forms import SubscriptionForm
 class SubscriptionView(generic.FormView):
     """ Basic subscription views """
     form_class = SubscriptionForm
-    template_name = 'django_mailjet/subscription.html'
+    template_name = 'django_mailjet/subscription_form.html'
     success_url = reverse_lazy('django_mailjet_subscription_success')
 
     def form_valid(self, form):
