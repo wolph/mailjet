8a688ae06f955c64dbed17691dfbe0b3c7640e0b
WoLpH <Rick@Fawo.nl>
2013-09-12 17:31:05 +0200

    added django support and tox for testing, disabled the django tests for now since they require an API key

--- setup.py
+++ setup.py
@@ -1,27 +1,25 @@
 import os
-from setuptools import setup
+import setuptools
 
 if os.path.isfile('README.rst'):
     long_description = open('README.rst').read()
 else:
     long_description = 'See http://pypi.python.org/pypi/mailjet/'
 
-setup(
-    name = 'mailjet',
-    version = '1.3.2',
-    author = 'Rick van Hattem',
-    author_email = 'Rick.van.Hattem@Fawo.nl',
-    description = '''mailjet is a django app to implement the mailjet REST
-        API''',
+setuptools.setup(
+    name='mailjet',
+    version='1.4.0',
+    author='Rick van Hattem',
+    author_email='Rick.van.Hattem@Fawo.nl',
+    description='mailjet is a django app to implement the mailjet REST API',
     url='https://github.com/WoLpH/mailjet',
-    license = 'BSD',
-    packages=['mailjet'],
+    license='BSD',
+    packages=setuptools.find_packages(),
     long_description=long_description,
     test_suite='nose.collector',
     tests_require=['nose'],
     classifiers=[
         'License :: OSI Approved :: BSD License',
     ],
-    data_files = [('', ['LICENSE'])],
 )
 
