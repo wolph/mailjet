e4a49f76b502a3bfbad5e3c9d809a7e3ab4777bc
Rick van Hattem <Wolph@wol.ph>
2015-05-24 14:13:31 +0200

    fixed tests

--- mailjet/settings.py
+++ mailjet/settings.py
@@ -4,19 +4,19 @@ The global settings prefix can be changed by modifying the `SETTINGS_PREFIX`
 variable. These settings can be overwritten by modifying the Django settings
 (in that case the `SETTINGS_PREFIX` is used) or by modifying this file.
 '''
-#SETTINGS_PREFIX = 'MAILJET_'
+# SETTINGS_PREFIX = 'MAILJET_'
 
 '''The Mailjet authentication data'''
-#API_KEY = None
-#SECRET_KEY = None
+# API_KEY = None
+# SECRET_KEY = None
 
 '''The timeout for the POST/GET requests'''
-#TIMEOUT = 10
+# TIMEOUT = 10
 
 '''The API version, currently only 0.1 is suported'''
-#VERSION = 0.1
+# VERSION = 0.1
 
 '''The url to fetch from, this is based on the version but this is not
 required'''
-#URL = 'https://api.mailjet.com/%(VERSION)s/'
+# URL = 'https://api.mailjet.com/%(VERSION)s/'
 
